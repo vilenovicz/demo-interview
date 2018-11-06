3efffc949b31d70b9d9f4529a83d9bec2010b47c
vilenovicz <vilenovicz@yandex.ru>
2018-11-06 22:05:20 +0400

    angle: refactoring

--- src/main/java/hands/Angle.java
+++ src/main/java/hands/Angle.java
@@ -7,14 +7,20 @@ package hands;
 import static java.lang.Math.abs;
 
 public class Angle {
+
+	private static final int CIRCLE_DEGREES = 360;
+	private static final int MINUTES_IN_HOUR = 60;
+	private static final int HOURS = 12;
+	private static final int DEGREES_BETWEEN_DIGITS = 30;
+
 	public static void main(String[] args) {
 		System.out.println(getCorner(12,30));
 	}
 	
 	private static float getCorner(int hours, int minutes) {
-		
-		int degreeMinutes = 360 / 60 * minutes;
-		int degreeHours = 360 / 12 * (hours % 12) + minutes*30/60;
+
+		int degreeMinutes = CIRCLE_DEGREES / MINUTES_IN_HOUR * minutes;
+		int degreeHours = CIRCLE_DEGREES / HOURS * (hours % HOURS) + minutes* DEGREES_BETWEEN_DIGITS / MINUTES_IN_HOUR;
 	
 		return abs(degreeHours - degreeMinutes);
 		
